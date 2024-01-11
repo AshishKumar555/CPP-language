8fa6172d39330be96b7894b66e51b1a7cd3ecadc
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-01-11 21:29:51 +0530

    using default parameter

--- 0.023Basic/3.cpp
+++ 0.023Basic/3.cpp
@@ -1,15 +1,26 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int x,y,z;
-    cout<<"Enter two numbers"<<endl;
-    cin>>x>>y;
-    z=x+y;
-    cout<<"The value of z is ==> "<<z;
-    return 0;
-}
-//-----using function------------------
-int sum(int a,int b,int c){
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y,z;
+//     cout<<"Enter two numbers"<<endl;
+//     cin>>x>>y;
+//     z=x+y;
+//     cout<<"The value of z is ==> "<<z;
+//     return 0;
+// }
+//-----using function------------------------------------------------------------------!
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace::std;
+
+void sum(int a,int b,int c=1){   //default parameter
     cout<<"The result is "<<a+b+c;
+}
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!"<<endl;
+    sum(10,20);
+    return 0;
 }
