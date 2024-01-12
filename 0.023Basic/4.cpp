865e6b7acc8096c25dcad0a9277f7a03cdd7009a
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-01-12 21:11:32 +0530

    functional approach

--- 0.023Basic/4.cpp
+++ 0.023Basic/4.cpp
@@ -9,3 +9,15 @@ int main()
     cout<<"The area of circle is ==>"<<area;
     return 0;
 }
+//------------------Function Approach--------------------
+
+        // #include <iostream>
+        // using namespace::std;
+        // float circleArea(float radius){
+        //     return 3.14*radius*radius;
+        // }
+        
+        // int main() {
+        //     cout<<"Area of circle is :"<<circleArea(10);
+        //     return 0;
+        // }
